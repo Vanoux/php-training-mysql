2aa1cd133c28762abfd14a996db2f620c98c2f43
Vanoux <vanessa.roux891@orange.fr>
2018-08-12 18:30:30 +0200

    deploy

---
+++ dbConnect.php
@@ -0,0 +1,11 @@
+<?php
+//mysqli gearhost
+// DEFINE("SERVER", "den1.mysql1.gear.host");
+// DEFINE("LOGIN", "reunion1");
+// DEFINE("MDP", "Im4oE85_-O7F");
+// DEFINE("BASE", "reunion1");
+// $connect=mysqli_connect(SERVER,LOGIN,MDP,BASE)or die("pb de connexion au serveur");
+
+//$pdo = new PDO('mysql:host=den1.mysql1.gear.host;dbname=reunion1','reunion1','Im4oE85_-O7F');
+$pdo = new PDO('mysql:host=den1.mysql1.gear.host;dbname=randoreunion','randoreunion','Xm097W!~oVZb');
+?>
